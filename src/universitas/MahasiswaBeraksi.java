69e93e46982599a3f7c6daef087f0e3480cb0ad0
Panjigumelar54 <134267886+Panjigumelar54@users.noreply.github.com>
2023-06-14 09:18:57 +0800

    Menjalankan Program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -1,10 +1,17 @@
 package universitas;
 
+
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+            
+    Mahasiswa mhs = new Mahasiswa("2110010551", " Panji Gumelar", " Teknik Informatika");
+    
+    
+        System.out.println("Nama: "+mhs.getNama());
+        System.out.println("NPM: "+mhs.getNpm());
+        System.out.println("Prodi: "+mhs.getProdi());
         
-        
-        
-        
-    }
+        System.out.println(mhs.setDetail());
+
+}
 }
